f0c94a6fd6e48494ac63b4b205b00d3db108f1e5
chris <chrs.har@gmail.com>
2017-12-20 19:46:46 +0100

    More stable version

--- src/web-app/audio/player.js
+++ src/web-app/audio/player.js
@@ -1,52 +1,59 @@
-import FileService from '../services/file-service.js';
-
 export default class AudioPlayer {
 
     constructor(audioContext) {
-        this.source = {};
+        this.context = audioContext;
+        this.source = this.context.createBufferSource();
+        this.gainNode = this.context.createGain();
+
         this.buffer = {};
-        this.startedAt = 0;
-        this.pausedAt = 0;
         this.lowpassFilter = {};
         this.highshelfFilter = {};
 
-        this.context = audioContext;
-        this.gainNode = this.context.createGain();
-
         this.paused = true;
+        this.startedAt = 0;
+        this.pausedAt = 0;
+
         this.lowpassConnected = false;
         this.highshelfConnected = false;
+        this.destinationConnected = false;
+
+        this.lowpassActive = false;
+        this.highshelfActive = false;
 
         this._initLowpass();
         this._initHighshelf();
     }
 
     _connectNodes() {
-
         this.source.connect(this.gainNode);
 
-        if (this.lowpassConnected && this.highshelfConnected) {
-              this.gainNode.connect(this.lowpassFilter);
-              this.lowpassFilter.connect(this.highshelfFilter);
-              this.highshelfFilter.connect(this.context.destination);
+        if (this.lowpassActive && this.highshelfActive) {
+            this.gainNode.connect(this.lowpassFilter);
+            this.lowpassFilter.connect(this.highshelfFilter);
+            this.highshelfFilter.connect(this.context.destination);
+            this.lowpassConnected = true;
+            this.highshelfConnected = true;
+            this.destinationConnected = true;
 
-          } else if (this.lowpassConnected && !this.highshelfConnected) {
-              this.gainNode.connect(this.lowpassFilter);
-              this.lowpassFilter.connect(this.context.destination);
-              this.lowpassConnected = true;
+        } else if (this.lowpassActive && !this.highshelfActive) {
+            this.gainNode.connect(this.lowpassFilter);
+            this.lowpassFilter.connect(this.context.destination);
+            this.lowpassConnected = true;
+            this.destinationConnected = true;
 
-          } else if (!this.lowpassConnected && this.highshelfConnected) {
-              this.gainNode.connect(this.highshelfFilter);
-              this.highshelfFilter.connect(this.context.destination);
-              this.highshelfConnected = true;
+        } else if (!this.lowpassActive && this.highshelfActive) {
+            this.gainNode.connect(this.highshelfFilter);
+            this.highshelfFilter.connect(this.context.destination);
+            this.highshelfConnected = true;
+            this.destinationConnected = true;
 
-          } else {
-              this.gainNode.connect(this.context.destination);
-              this.destinationConnected = true;
-          }
+        } else {
+            this.gainNode.connect(this.context.destination);
+            this.destinationConnected = true;
+        }
     }
 
-    loadAudioFile (audioFile) {
+    loadAudioFile(audioFile) {
         this.buffer = audioFile.audioBuffer
     }
 
@@ -119,43 +126,38 @@ export default class AudioPlayer {
     }
 
     _disconnectNodes() {
-
-        if(this.destinationConnected) {
+        if (this.destinationConnected) {
             this.source.disconnect(0);
             this.gainNode.disconnect(0);
             this.destinationConnected = false;
         }
         if (this.lowpassConnected) {
             this.lowpassFilter.disconnect(0);
+            this.lowpassConnected = false;
         }
         if (this.highshelfConnected) {
             this.highshelfFilter.disconnect(0);
+            this.lowpassConnected = false;
         }
     }
 
     toggleLowpass() {
-
-        this._disconnectNodes();
-
-        if (this.lowpassConnected) {
-            this.lowpassConnected = false;
+        if (this.lowpassActive) {
+            this.lowpassActive = false;
         } else {
-            this.lowpassConnected = true;
+            this.lowpassActive = true;
         }
-
+        this._disconnectNodes();
         this._connectNodes();
     }
 
     toggleHighshelf() {
-
-        this._disconnectNodes();
-
-        if (this.highshelfConnected) {
-            this.highshelfConnected = false;
+        if (this.highshelfActive) {
+            this.highshelfActive = false;
         } else {
-            this.highshelfConnected = true;
+            this.highshelfActive = true;
         }
-
+        this._disconnectNodes();
         this._connectNodes();
     }
 }
