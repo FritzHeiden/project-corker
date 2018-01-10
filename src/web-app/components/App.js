8162f701cee91651d6369407669fbd4597d644da
Fritz Heiden <fritz.heiden@mailbox.org>
2018-01-10 17:59:39 +0100

    implemented MidiController class

--- src/web-app/components/App.js
+++ src/web-app/components/App.js
@@ -11,6 +11,7 @@ import FileService from '../services/file-service.js'
 
 import Background from './designObjects/Background.js'
 import Options from './designObjects/Options.js'
+import MidiController from '../controller/midi-controller'
 //import BackgroundImage from 'react-svg-loader!../svg/Background.svg'; // just works with folder.js
 
 export default class App extends React.Component {
@@ -20,6 +21,12 @@ export default class App extends React.Component {
 
         this._fileService = new FileService('localhost', 2345)
         this._audioContext = new (window.AudioContext || window.webkitAudioContext)()
+      this._midiController = new MidiController()
+      this._midiController.initialize()
+      this._midiController.listenOnCrossFaderChange(value => {console.log('cross fader action!', value)})
+      this._midiController.listenOnFxButtonChange((value, mode) => console.log('fx button', value, 'mode:', mode))
+      this._midiController.listenOnFxKnob4Column1Change((value, mode) => console.log('fx knob 4 row 1', value, 'mode:', mode))
+      this._midiController.listenOnBrowseKnobChange((value, mode) => console.log('browse knob!', value, 'mode:', mode))
     }
 
     render() {
